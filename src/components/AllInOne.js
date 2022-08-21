aa02f1a84209b524bcb25c6edd8dc14b401028ae
Goga <gogaimerlishvili1@gmail.com>
2022-08-21 21:15:40 +0400

    added next section for notifications

---
+++ src/components/AllInOne.js
@@ -0,0 +1,83 @@
+import { CheckIcon } from "@heroicons/react/outline";
+import React from "react";
+
+const AllInOne = () => {
+  return (
+    <div className="w-full my-32 md:my-[-10%]">
+      <div className="max-w-[1240px] mx-auto px-2">
+        <h2 className="text-5xl font-bold text-center">All-In-One Platform</h2>
+        <p className="text-2xl py-8 text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum
+        </p>
+
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 pt-4">
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div>
+              <CheckIcon className="w-7 mr-4 text-green-600" />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Notifications</h3>
+              <p className="text-lg pt-2 pb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllInOne;
